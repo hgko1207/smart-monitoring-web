9be48256e681e3824e0a9c8b537b7531308d2f80
hgko1207 <khkkhk1207@gmail.com>
2020-05-19 11:07:55 +0900

    layout 구현

--- smart-web/src/main/java/net/woori/start/controller/HomeController.java
+++ smart-web/src/main/java/net/woori/start/controller/HomeController.java
@@ -1,8 +1,19 @@
 package net.woori.start.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	
+	@GetMapping("/")
+    public String index(Model model) {
+		 return "redirect:home";
+    }
 
+	@GetMapping("home")
+    public String home(Model model) {
+        return "home";
+    }
 }
