282e0eef22d622d61adf52623c04822e3c7de967
hgko1207 <khkkhk1207@gmail.com>
2020-05-25 16:31:09 +0900

    Database 설정

--- smart-web/src/main/java/net/woori/start/domain/db/User.java
+++ smart-web/src/main/java/net/woori/start/domain/db/User.java
@@ -1,8 +1,58 @@
 package net.woori.start.domain.db;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import lombok.Data;
+import net.woori.start.domain.Domain;
 
+/**
+ * 사용자 관리 도메인
+ * 
+ * @author hgko
+ *
+ */
+@Entity
+@Table(name = "tb_user")
 @Data
-public class User {
+public class User implements Domain {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	/** 사용자ID */
+	@Column(nullable = false, length = 20)
+	private String userId;
 
+	@Column(nullable = false, length = 24)
+	private String password;
+
+	@Column(nullable = false, length = 20)
+	private String name;
+	
+	@CreationTimestamp
+	private LocalDateTime createDate;
+	
+	@UpdateTimestamp
+	private LocalDateTime updateDate;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private UserRole role;
+	
+	public enum UserRole {
+		ADMIN, GUEST;
+	}
 }
